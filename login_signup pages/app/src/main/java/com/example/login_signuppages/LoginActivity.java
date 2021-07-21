a7c181980029e07c66de73f9d1fc6015d056f56e
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-07-21 17:46:06 +0900

    php 환경 설정 완료_vscode 이용+ localhost로 접속해 확인하기

--- login_signup pages/app/src/main/java/com/example/login_signuppages/LoginActivity.java
+++ login_signup pages/app/src/main/java/com/example/login_signuppages/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.login_signuppages;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registerButton=(TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                Intent registerIntent=new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- login_signup pages/app/src/main/java/com/example/login_signuppages/RegisterActivity.java
+++ login_signup pages/app/src/main/java/com/example/login_signuppages/RegisterActivity.java
@@ -1,13 +1,9 @@
 package com.example.login_signuppages;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.fragment.app.DialogFragment;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,19 +11,5 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-    }
-
-    public void showDatePicker(View view) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(),"datePicker");
-    }
-
-    public void processDatePickerResult(int year, int month, int day){
-        String month_string = Integer.toString(month+1);
-        String day_string = Integer.toString(day);
-        String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
-
-        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
     }
 }
