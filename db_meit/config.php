079f15368769de0e7d01fb38ac78470a78e84222
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-08-19 00:05:39 +0900

    modified

--- db_meit/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("DB_HOST", "localhost");
-	define("DB_USER", "zhenying2");
-	define("DB_PASSWORD", "maria1816@");
-	define("DB_NAME", "zhenying2");
-
-	define("GOOGLE_API_KEY", "AAAA5cD1u0g:APA91bEXTA18-khvVU9--e4rXCBRT03M_fCwViQdTcosmvXUTU6LDHV2JqjFpnFL1n78G5RHk4GgWQkDBSZKq9BY-mCF6byHJIG7PbzlKRqEyuR92nZpzlhstSy4caanIy2AP01F83mE"); 
-
-?>
