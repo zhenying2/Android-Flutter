8e0de4f10a12e96a3eb1ae5eb843be3685afcc35
zhenying2 <khappy9888@sookmyung.ac.kr>
2022-01-18 19:42:31 +0900

    네비게이션 탭 설정 완료
    netflex 클론코딩

---
+++ Flutter/Jinplex/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:jinplex/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  late TabController controller;
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Jinplex',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child:Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              Container(child: Center(child: Text('home'),),),
+              Container(child: Center(child: Text('search'),),),
+              Container(child: Center(child: Text('save'),),),
+              Container(child: Center(child: Text('more'),),),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
