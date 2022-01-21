22a8184b54b1182a43ad22e8a3702bd5dd719e28
zhenying2 <khappy9888@sookmyung.ac.kr>
2022-01-21 11:30:49 +0900

    상세보기 페이지

--- Flutter/Jinplex/lib/widget/carousel_slider.dart
+++ Flutter/Jinplex/lib/widget/carousel_slider.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jinplex/model/model_movie.dart';
+import 'package:jinplex/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget{
   final List<Movie> movies;
@@ -102,7 +103,14 @@ class _CarouselImageState extends State<CarouselImage>{
                     children: <Widget>[
                       IconButton(
                         icon: Icon(Icons.info),
-                        onPressed: (){},
+                        onPressed: (){
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                            fullscreenDialog: true,
+                            builder: (BuildContext context){
+                              return DetailScreen(movie: movies[_currentPage],
+                              );
+                            }));
+                        },
                       ),
                       Text(
                         '정보',
