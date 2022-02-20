cb3138f690748cac6b7b0c1a9c3b86b315db8a97
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-02-21 00:02:16 +0900

    당근마켓 클론코딩_이미지 슬라이드
    상세페이지

--- Flutter/carrot_market/lib/page/detail.dart
+++ Flutter/carrot_market/lib/page/detail.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,35 +12,96 @@ class DetailContentView extends StatefulWidget{
 
 class _DetailContentViewState extends State<DetailContentView>{
   late Size size;
+  late List<Map<String,String>> imgList;
+  late int _current;
+
   @override
   void didChangeDependencies(){
     super.didChangeDependencies();
     size=MediaQuery.of(context).size;
+    _current=0;
+    imgList=[
+      {"id": "0", "url":widget.data["image"]!},
+      {"id": "1", "url":widget.data["image"]!},
+      {"id": "2", "url":widget.data["image"]!},
+      {"id": "3", "url":widget.data["image"]!},
+      {"id": "4", "url":widget.data["image"]!},
+    ];
   }
   PreferredSizeWidget _appbarWidget(){
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(onPressed: (){
+        Navigator.pop(context);
+      },
+          icon: Icon(Icons.arrow_back,color: Colors.white)),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-        IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.share,color: Colors.white)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.more_vert,color: Colors.white)),
       ],
     );
   }
 
   Widget _bodyWidget(){
     return Container(
-      child: Hero(
-        tag: widget.data["cid"]!,
-        child: Image.asset(
-            widget.data["image"]!,
-            width:size.width,
-            fit: BoxFit.fill
+      child: Stack(
+        children: [
+        Hero(
+          tag: widget.data["cid"]!,
+          child:
+            CarouselSlider(
+              items:imgList.map((map){
+                return Image.asset(
+                    map["url"]!,
+                    width:size.width,
+                    fit: BoxFit.fill,
+                );
+              }).toList(),
+              options: CarouselOptions(
+                height: size.width,
+                initialPage: 0,
+                enableInfiniteScroll: false,
+                viewportFraction: 1,
+                onPageChanged: (index,reason){
+                  setState(() {
+                    _current=index;
+                  });
+                }
+              ),
+            ),
+          ),
+        Positioned(
+          bottom: 0,
+          left:0,
+          right:0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.map((map){
+              return Container(
+                width: 10.0,
+                height: 10.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == int.parse(map["id"]!)
+                    ? Colors.white : Colors.white.withOpacity(0.4),
+                ),
+              );
+            }).toList(),
+          ),
         ),
-      ),
+      ],),
     );
   }
 
+  Widget _bottomBarWidget(){
+   return Container(
+     width: size.width,
+     height: 55,
+     color: Colors.red,
+   );
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
