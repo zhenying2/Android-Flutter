0605a04c0575b0801baaf7d1943294e89d534868
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-02-20 17:06:39 +0900

    당근마켓 클론_홈 tabbar 완성

---
+++ Flutter/carrot_market/lib/page/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  PreferredSizeWidget _appbarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () {
+          print("click");
+        },
+        child: Row(
+          children: [
+            Text("아라동"),
+            Icon(Icons.arrow_drop_down),
+          ],
+        ),
+      ),
+      elevation: 1,
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
+        IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/svg/bell.svg",
+              width: 22,
+            )),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbarWidget(),
+      body: Container(),
+      bottomNavigationBar: Container(),
+    );
+  }
+}
