cbfbee5c13acecc2e8b5c8c64928106a4b40fc70
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-02-20 18:21:22 +0900

    당근마켓 클론_bottom navibar

--- Flutter/carrot_market/lib/page/home.dart
+++ Flutter/carrot_market/lib/page/home.dart
@@ -11,10 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String,String>> datas=[];
+  late int _currentPageIndex;
 
   @override
   void initState(){
     super.initState();
+    _currentPageIndex=0;
     datas=[
       {
         "image" : "assets/images/ara-1.jpg",
@@ -145,12 +147,45 @@ class _HomeState extends State<Home> {
     );
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(String icon, String label){
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding:const EdgeInsets.only(bottom: 5),
+        child:SvgPicture.asset("assets/svg/${icon}_off.svg",width: 22),
+      ),
+      label: label,
+    );
+  }
+
+  Widget _bottomNavigationBarWidget(){
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index){
+        print(index);
+        setState(() {
+          _currentPageIndex=index;
+        });
+      },
+      currentIndex: _currentPageIndex,
+      selectedFontSize: 12,
+      selectedItemColor: Colors.black,
+      selectedLabelStyle: TextStyle(color: Colors.black),
+      items: [
+        _bottomNavigationBarItem("home", "홈"),
+        _bottomNavigationBarItem("notes", "동네생활"),
+        _bottomNavigationBarItem("location", "내 근처"),
+        _bottomNavigationBarItem("chat", "채팅"),
+        _bottomNavigationBarItem("user", "나의 당근")
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbarWidget(),
       body: _bodyWidget(),
-      //bottomNavigationBar: Container(),
+      bottomNavigationBar: _bottomNavigationBarWidget(),
     );
   }
 }
