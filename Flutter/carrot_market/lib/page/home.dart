9da33658f72214453f9a7dcc1e42188b9fe38c99
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-02-20 22:18:53 +0900

    당근마켓 클론_상세페이지(사진꽉차게)
    Hero 모션 효과+

--- Flutter/carrot_market/lib/page/home.dart
+++ Flutter/carrot_market/lib/page/home.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_market/page/detail.dart';
 import 'package:carrot_market/repository/contents_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -84,46 +85,60 @@ class _HomeState extends State<Home>{
     return ListView.separated(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       itemBuilder: (BuildContext _context, int index){
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius:BorderRadius.all(Radius.circular(10)),
-                child: Image.asset(
-                  datas[index]["image"]!,
-                  width: 100,
-                  height: 100,
-                ),
-              ),
-              Expanded(
-                child:Container(
-                  height: 100,
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(datas[index]["title"]!,style: TextStyle(fontSize: 15),overflow: TextOverflow.ellipsis,),
-                      SizedBox(height: 5),
-                      Text(datas[index]["location"]!,style: TextStyle(fontSize: 12,color: Colors.black.withOpacity(0.3)),),
-                      SizedBox(height: 5),
-                      Text(datas[index]["price"]!,style: TextStyle(fontWeight: FontWeight.w500),),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            SvgPicture.asset("assets/svg/heart_off.svg",width: 13,height: 13,),
-                            SizedBox(width: 5,),
-                            Text(datas[index]["likes"]!),
-                          ],
-                        ),
-                      ),
-                    ],
+        return GestureDetector(
+          onTap: (){
+            Navigator.push(context,
+                MaterialPageRoute(builder:(BuildContext context){
+              return DetailContentView(
+                  data: datas[index],
+              );
+            }));
+            print(datas[index]["title"]);
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius:BorderRadius.all(Radius.circular(10)),
+                  child: Hero(
+                    tag: datas[index]["cid"]!,
+                    child: Image.asset(
+                      datas[index]["image"]!,
+                      width: 100,
+                      height: 100,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child:Container(
+                    height: 100,
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(datas[index]["title"]!,style: TextStyle(fontSize: 15),overflow: TextOverflow.ellipsis,),
+                        SizedBox(height: 5),
+                        Text(datas[index]["location"]!,style: TextStyle(fontSize: 12,color: Colors.black.withOpacity(0.3)),),
+                        SizedBox(height: 5),
+                        Text(datas[index]["price"]!,style: TextStyle(fontWeight: FontWeight.w500),),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              SvgPicture.asset("assets/svg/heart_off.svg",width: 13,height: 13,),
+                              SizedBox(width: 5,),
+                              Text(datas[index]["likes"]!),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
